2bdbc9eb676d73f7533b9f8035961b39d3b0690d
Jeampier <jeampier.cornejo@tecsup.edu.pe>
2022-10-17 11:37:53 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by jeampier on 10/17/22.
+//  Copyright © 2022 miempresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
